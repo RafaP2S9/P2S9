d465883e70f9e03903346cc684c02528e4f7255c
RafaP2S9 <142831906+RafaP2S9@users.noreply.github.com>
2023-09-12 05:28:38 +0000

    🌟feature:Server directory Se mueven los archivos de backend al

--- server/bin/www.js
+++ server/bin/www.js
@@ -86,5 +86,5 @@ function onListening() {
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
-  debug('🔊Listening on ' + bind);
+  debug('Listening on ' + bind);
 }

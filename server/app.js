4d9407dbca6a180cc916d4214c6f31147a5239fd
RafaP2S9 <142831906+RafaP2S9@users.noreply.github.com>
2023-09-13 04:58:49 +0000

    🌟Enhacement app.js errores

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var debug = require('debug')('p2s9:server');
-
+var debug = require('debug')('DWPC-2:server');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
 // CReando la instancia express
 var app = express();
 
@@ -16,18 +16,25 @@ var app = express();
 debug(`🔊 Ruta actual de app: ${__dirname,'views'}`);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+
 // se establecen los middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 //Crea un server de archivos estaticos 
 app.use(express.static(path.join(__dirname, '..','public')));
+
 // me ayudan enrutar algo  con un /
 app.use('/', indexRouter);
+
 // Activa "usersRouter" cuando se 
 // solicita "/users"
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+
 //app.use('/author',(req, res)=>{
 // res.json({mainDeveloper: "Rafael elizalde"})
 //});
